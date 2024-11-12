02afa0c910c181cb16acf2c5c2ac3c843d4ea727
Clément DE TEMMERMAN <clement.de-temmerman@student.junia.com>
2024-11-12 11:14:23 +0100

    Created WebApplication.java

--- lab05-web/controller/SagasController.java
+++ lab05-web/controller/SagasController.java
@@ -1,5 +1,16 @@
-import SagaService;
+package junia.lab05.web.controller;
+
+import junia.lab05.core.service.SagaService;
+
 @Controller
 @RequestMapping("sagas")
 public class SagasController {
+
+    private final SagaService sagaService;
+
+    @Autowired
+    public SagasController(SagaService sagaService) {
+        this.sagaService = sagaService;
+    }
+
 }
